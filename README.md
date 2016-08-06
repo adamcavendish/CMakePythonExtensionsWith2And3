016c979c1ee24581729dc44654b028c188ffd23a
Adam Cavendish <GetbetterABC@yeah.net>
2016-08-06 09:11:47 +0800

    Fix string null issue with CMake variables

--- README.md
+++ README.md
@@ -4,10 +4,25 @@ Build Python 2 and 3 Extensions with CMake
 
 ---
 
-Quick start:
+# Requirements
+
+cmake >= 2.8
+
+# Info
+
+`FindPython.cmake` Module requires the a newer [FindPythonInterp.cmake](https://github.com/Kitware/CMake/blob/master/Modules/FindPythonInterp.cmake)
+and [FindPythonLibs.cmake](https://github.com/Kitware/CMake/blob/master/Modules/FindPythonLibs.cmake) from CMake (at least 3.4)
+
+# Quick start:
 
 ```
-mkdir build
+# Get latest FindPythonInterp.cmake and FindPythonLibs.cmake
+cd cmake/
+wget -c "https://raw.githubusercontent.com/Kitware/CMake/master/Modules/FindPythonInterp.cmake"
+wget -c "https://raw.githubusercontent.com/Kitware/CMake/master/Modules/FindPythonLibs.cmake"
+cd ../
+
+mkdir -p build
 cd build
 cmake ..
 make
@@ -19,3 +34,4 @@ python2 greet2.py
 cd ../py3/
 python3 greet3.py
 ```
+
--- cmake/FindPython.cmake
+++ cmake/FindPython.cmake
@@ -43,8 +43,8 @@ function(find_python min_version found executable
   # var     (variable): variable to set to if the environment variable is available
   # env_var (value)   : the environment variable name
   macro(set_from_env var env_var)
-    if(DEFINED ENV{${env_var}})
-      set(${var} $ENV{${env_var}})
+    if(DEFINED ENV{"${env_var}"})
+      set(${var} "$ENV{${env_var}}")
     endif()
   endmacro()
 
@@ -53,7 +53,7 @@ function(find_python min_version found executable
   # val (value)   : value to set to the variable if empty
   macro(set_ifndef var val)
     if(NOT ${var})
-      set(${var} ${val})
+      set(${var} "${val}")
     endif()
   endmacro()
 
@@ -86,9 +86,9 @@ function(find_python min_version found executable
 
     find_package(PythonLibs "${_version_combined}")
     if(PYTHONLIBS_FOUND)
-      set_ifndef(_libraries ${PYTHON_LIBRARIES})
-      set_ifndef(_include_dirs ${PYTHON_INCLUDE_PATH})
-      set_ifndef(_include_dirs ${PYTHON_INCLUDE_DIRS})
+      set_ifndef(_libraries "${PYTHON_LIBRARIES}")
+      set_ifndef(_include_dirs "${PYTHON_INCLUDE_PATH}")
+      set_ifndef(_include_dirs "${PYTHON_INCLUDE_DIRS}")
 
       # unset the variables to cleanup side effects from calling find_package
       unset(PYTHONLIBS_FOUND)
@@ -105,50 +105,50 @@ function(find_python min_version found executable
   endif()
 
   if(NOT _found AND _executable)
-    set_ifndef(_version_combined ${min_version})
-    string(REPLACE "." "" _version_combined_no_dots ${_version_combined})
+    set_ifndef(_version_combined "${min_version}")
+    string(REPLACE "." "" _version_combined_no_dots "${_version_combined}")
 
     # tackle the libraries
-    execute_process(COMMAND ${_executable} -c
+    execute_process(COMMAND "${_executable}" -c
                     "from distutils.sysconfig import get_config_var; print(get_config_var('LIBDIR'))"
                     RESULT_VARIABLE __
                     OUTPUT_VARIABLE _unchecked_libraries
                     OUTPUT_STRIP_TRAILING_WHITESPACE)
     find_library(_libraries_found
                  NAMES
-                   python${_version_combined_no_dots}
-                   python${_version_combined}mu
-                   python${_version_combined}m
-                   python${_version_combined}u
-                   python${_version_combined}
-                 PATHS ${_unchecked_libraries}
+                   "python${_version_combined_no_dots}"
+                   "python${_version_combined}mu"
+                   "python${_version_combined}m"
+                   "python${_version_combined}u"
+                   "python${_version_combined}"
+                 PATHS "${_unchecked_libraries}"
                  NO_SYSTEM_ENVIRONMENT_PATH)
     find_library(_libraries_found
                  NAMES
-                   python${_version_combined_no_dots}
-                   python${_version_combined}
+                   "python${_version_combined_no_dots}"
+                   "python${_version_combined}"
                  NO_SYSTEM_ENVIRONMENT_PATH
                  PATH_SUFFIXES "python${_version_combined}/config")
     # clean up find_library side effects
-    set(_libraries ${_libraries_found})
+    set(_libraries "${_libraries_found}")
     unset(_libraries_found CACHE)
 
     # tackle the include_dirs
-    execute_process(COMMAND ${_executable} -c
+    execute_process(COMMAND "${_executable}" -c
                     "from distutils.sysconfig import get_python_inc; print(get_python_inc())"
                     RESULT_VARIABLE __
                     OUTPUT_VARIABLE _unchecked_include_dirs
                     OUTPUT_STRIP_TRAILING_WHITESPACE)
     find_path(_include_dirs_found
-              NAMES Python.h
-              PATHS ${_unchecked_include_dirs}
+              NAMES "Python.h"
+              PATHS "${_unchecked_include_dirs}"
               PATH_SUFFIXES
-                python${_version_combined}mu
-                python${_version_combined}m
-                python${_version_combined}u
-                python${_version_combined})
+                "python${_version_combined}mu"
+                "python${_version_combined}m"
+                "python${_version_combined}u"
+                "python${_version_combined}")
     # clean up find_library side effects
-    set(_include_dirs ${_include_dirs_found})
+    set(_include_dirs "${_include_dirs_found}")
     unset(_include_dirs_found CACHE)
   endif()
 
@@ -160,7 +160,7 @@ function(find_python min_version found executable
   if(_found)
     if(CMAKE_HOST_UNIX)
       # setup package_path
-      execute_process(COMMAND ${_executable} -c
+      execute_process(COMMAND "${_executable}" -c
                       "from distutils.sysconfig import get_python_lib; print(get_python_lib())"
                       RESULT_VARIABLE __
                       OUTPUT_VARIABLE _full_packages_path
@@ -181,13 +181,15 @@ function(find_python min_version found executable
   # return
   set(${found} "${_found}" PARENT_SCOPE)
   if(_found)
-    set(${executable} "${_executable}" CACHE FILEPATH "Path to Python interpretor")
+    set(${executable}     "${_executable}"     CACHE FILEPATH "Path to Python interpretor")
     set(${version_string} "${_version_string}" CACHE STRING "Python version")
-    set(${version_major} "${_version_major}" PARENT_SCOPE)
-    set(${version_minor} "${_version_minor}" PARENT_SCOPE)
-    set(${version_patch} "${_version_patch}" PARENT_SCOPE)
-    set(${libraries} "${_libraries}" CACHE FILEPATH "Path to Python library")
-    set(${include_dirs} "${_include_dirs}" CACHE PATH "Python include dir")
-    set(${packages_dir} "${_packages_dir}" CACHE STRING "Python partial packages dir for install")
+    set(${version_major}  "${_version_major}"  PARENT_SCOPE)
+    set(${version_minor}  "${_version_minor}"  PARENT_SCOPE)
+    set(${version_patch}  "${_version_patch}"  PARENT_SCOPE)
+    set(${libraries}      "${_libraries}"      CACHE FILEPATH "Path to Python library")
+    set(${include_dirs}   "${_include_dirs}"   CACHE PATH "Python include dir")
+    set(${packages_dir}   "${_packages_dir}"   CACHE STRING "Python partial packages dir for install")
+  else()
+    message(FATAL_ERROR "Cannot find required python >= version ${min_version}")
   endif()
 endfunction(find_python)
